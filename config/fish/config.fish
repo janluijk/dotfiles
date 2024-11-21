42a9787239142f5240585f8e108552269914ceb3
Jan Luijk <luijk.jan@gmail.com>
2024-11-21 15:20:29 +0100

    Update alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,7 +4,7 @@ set -gx EDITOR nvim
 alias vim=nvim
 
 # Dotfiles
-alias dotfiles='cd $HOME/dotfiles/config'
+alias dotfiles='cd $HOME/dotfiles & nvim'
 
 # Nvim config
 alias vimrc='cd $HOME/dotfiles/config/nvim & nvim'
@@ -12,13 +12,9 @@ alias vimrc='cd $HOME/dotfiles/config/nvim & nvim'
 # To change alias, open this file
 alias aliaschanger='nvim $HOME/dotfiles/config/fish/config.fish'
 
-# Prints my wallpapers directory
-alias mywallpapers='cd $HOME/pictures/wallpapers'
-
-# Prints screenshots directory
-alias myscreenshots='cd $HOME/pictures/screenshots'
-
 # Obsidian 
 alias oo='cd $HOME/personal/vault/personal'
+
+alias config='cd $HOME/dotfiles/config & nvim'
 
 starship init fish | source
